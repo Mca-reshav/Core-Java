002daf77da6a610aa62d3209a8b5994575b9ccf3
Reshav Kumar Gupta <111631321+Mca-reshav@users.noreply.github.com>
2023-02-26 23:00:22 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+        public static void main(String[] args){
+            System.out.println("Do While Loop");
+            int i=50;
+            do{
+                System.out.println(i);
+                i-=5;
+            }while(i>5);
+        }
+    }
+
